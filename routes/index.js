4586d6e55a5f56d1308d20db8ff3d6d8126c96eb
Aziz Moiz <aziz@bongolive.co.tz>
2021-01-18 08:39:43 +0300

    course continues

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+const speakersRoute = require('./speakers');
+const feedbackRoute = require('./feedback');
+
+module.exports = (params) => {
+    const {
+        speakersService
+    } = params;
+    router.get('/', async (request, response) => {
+        const topSpeakers = await speakersService.getList();
+        console.log(topSpeakers);
+        response.render('layout', {
+            pageTitle: 'Welcome',
+            template: 'index',
+            topSpeakers,
+        });
+    });
+
+    router.use('/speakers', speakersRoute(params));
+    router.use('/feedback', feedbackRoute(params));
+    return router;
+};
